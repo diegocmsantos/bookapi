ea921f691c8d8672d5fe0972bd2e905e98d51a7f
Diego Maia <diegocmsantos@gmail.com>
2016-04-06 09:31:32 -0300

    Unit Tests using Mocha

---
+++ tests/bookControllerTest.js
@@ -0,0 +1,36 @@
+var should = require('should'),
+    sinon = require('sinon');
+
+describe('Book Controller Tests', function() {
+
+  describe('Post Tests', function() {
+
+    it('should not allow an empty title on post', function() {
+
+      var Book = function(book) {
+        this.save = function(){}
+      }
+
+      var req = {
+        body: {
+          author: 'Diego Maia'
+        }
+      }
+
+      var res = {
+        status: sinon.spy(),
+        send: sinon.spy()
+      }
+
+      var bookController = require('../controllers/bookController')(Book);
+
+      bookController.post(req, res);
+
+      res.status.calledWith(400).should.equal(true, 'Bad Status ' + res.status.args[0][0]);
+      res.send.calledWith('Title is required').should.equal(true);
+
+    })
+
+  })
+
+})
