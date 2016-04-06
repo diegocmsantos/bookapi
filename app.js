737ded6739948acc7539a89d8ea43fabe981e372
Diego Maia <diegocmsantos@gmail.com>
2016-04-06 08:58:40 -0300

    HTTP verbs including PUT and PATCH, Middlewares

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
+var db = mongoose.connect('mongodb://localhost/bookapi');
+var Book = require('./models/book');
 var app = new express();
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+
+bookRouter = require('./routes/bookRoutes')(Book);
+
+app.use('/api/books', bookRouter);
 
 app.get('/', function(req,res) {
 
